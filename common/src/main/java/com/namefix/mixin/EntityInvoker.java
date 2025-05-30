2af0ec1083d155bd9138eceddd9df70ea16e6737
Namefix <41016273+Namefix@users.noreply.github.com>
2025-05-30 12:31:06 +0300

    Improved laser projectiles
    - Improved laser projectile collisions
    - Improved laser projectile update rate
    - Tweaked projectile speed of laser gun items

---
+++ common/src/main/java/com/namefix/mixin/EntityInvoker.java
@@ -0,0 +1,12 @@
+package com.namefix.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Entity.class)
+public interface EntityInvoker {
+	@Invoker("collide")
+	Vec3 zapinators$invokeCollide(Vec3 vec3);
+}
